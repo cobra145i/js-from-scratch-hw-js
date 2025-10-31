7bbb4dcbf5ce72112d044eb32f2354da5dc987c4
Igor Trushin <cobra145@mail.ru>
2025-10-31 13:05:02 +0300

    task lesson-04/02

--- lesson-04/01.js
+++ lesson-04/01.js
@@ -13,4 +13,9 @@
 includesElement([1, 2, 3], 2)  // должен вернуть `true`.
 */
 
-function includesElement() {}
+function includesElement(arr, element) {
+    for (let i = 0; i < arr.length; i++)
+        if (arr[i] == element) {
+            return true
+        }
+}
