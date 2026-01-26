80eb452023cc8c56adda5b6b1fec97255e333697
Igor Trushin <cobra145@mail.ru>
2026-01-26 09:22:39 +0300

    task 06-01

--- lesson-06/01.js
+++ lesson-06/01.js
@@ -23,4 +23,4 @@ const oddNumbers = filter(numbers, (element, index) => {
 console.log(oddNumbers) // Должен вывести: [1, 3, 5]
 */
 
-const filter = () => {}
+const callback = filter(numbers, (element, index) => true);
